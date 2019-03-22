538e5202e275123f071b1bbca7fda31381fda2d5
JohnCUIT <632913662@qq.com>
2019-03-22 18:33:00 +0800

    修改程序主框架，增加事件循环函数

--- main.py
+++ main.py
@@ -1,38 +1,34 @@
 #-*- coding: UTF-8 -*-
 import curses
 import locale
+import subprocess
 
 locale.setlocale(locale.LC_ALL, '')
 
-stdscr = curses.initscr()
+def execmd(command):
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    lines = []
+    for line in iter(p.stdout.readline, b''):
+        line = line.strip('\n')
+        lines.append(line)
+    return lines
 
-def get_ch_and_continue():
-    '''''演示press any key to continue'''
-    global stdscr
-    #设置nodelay，为0时会变成阻塞式等待
-    stdscr.nodelay(0)
-    #输入一个字符
-    ch=stdscr.getch()
-    #重置nodelay,使得控制台可以以非阻塞的方式接受控制台输入，超时1秒
-    stdscr.nodelay(1)
-    return True
+def event_loop(stdscr):
+    while 1:
+        stdscr.nodelay(0)
+        c = stdscr.getch()
+        if c == ord('p'):
+                outline = execmd("./merge.sh x")
+        elif c == ord('c'):
+                stdscr.erase()
+        elif c == ord('q'):
+                break  # Exit the while()
 
-def initScreen():
-    #使用颜色首先需要调用这个方法
-    curses.start_color()
-    curses.use_default_colors()
-    curses.init_pair(1, curses.COLOR_GREEN, -1)
-    #关闭屏幕回显
-    curses.noecho()
-    #输入时不需要回车确认
-    curses.cbreak()
-
-def exitScreen():
+def exitScreen(mainwin):
     '''控制台重置'''
-    global stdstr
     #恢复控制台默认设置（若不恢复，会导致即使程序结束退出了，控制台仍然是没有回显的）
     curses.nocbreak()
-    stdscr.keypad(0)
+    mainwin.keypad(0)
     curses.echo()
     #结束窗口
     curses.endwin()
@@ -41,6 +37,14 @@ class WidgetBase:
     def __init__(self, parent=0, x=0, y=0, w=0, h=0):
         if 0 == parent:
             self.screen = curses.initscr()
+            curses.start_color()
+            curses.use_default_colors()
+            curses.init_pair(1, curses.COLOR_GREEN, -1)
+            #关闭屏幕回显
+            curses.noecho()
+            #输入时不需要回车确认
+            curses.cbreak()
+            self.screen.keypad(1)
         else:
             self.screen = parent.screen.derwin(h, w, y, x)
             self.parent = parent
@@ -72,10 +76,9 @@ class MenuList(WidgetBase):
         self.currentIndex = index
         self.Display(1, index+1, '->')
 
-
 if __name__=='__main__':
     try:
-        initScreen()
+#        desktop = initScreen()
         win = WidgetBase()
         win.Display(30,1,'Svn Tool Box V1.0',1)
         win.Border()
@@ -84,19 +87,22 @@ if __name__=='__main__':
         menuWin.AppendItem(["1.更新"])
         menuWin.AppendItem(["2.合并"])
         menuWin.AppendItem(["3.提交"])
-        menuWin.SetCurrentItem(1)
+        menuWin.AppendItem(["4.浏览"])
+        menuWin.SetCurrentItem(4)
 
         mainWin = WidgetBase(win, 17, 3, 100, 30)
         mainWin.Border()
-        testWin = WidgetBase(mainWin, 2, 2, 40, 4)
-        testWin.Border()
-        testWin.Display(3,2, str(testWin.screen.getbegyx()))
-#        print(str(mainWin.screen.getmaxyx()[1]))
+        mainWin.Display(1,2, execmd("cat merge.sh")[2])
+#        testWin = WidgetBase(mainWin, 2, 2, 80, 4)
+#        testWin.Border()
+#        testWin.Display(3,1, execmd("ls -l")[2])
 
 
-        get_ch_and_continue()
+#        get_ch_and_continue(win.screen)
+        event_loop(win.screen)
     except Exception,e:
         raise e
     finally:
-        exitScreen()
+#        testWin.Display(3,2, execmd("ls -l")[3])
+        exitScreen(win.screen)
 
